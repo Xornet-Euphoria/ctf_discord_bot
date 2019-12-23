2bb1213dc5e2b4053c9e2abb95a1ab18db3b40b3
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2019-12-23 17:54:39 +0900

    quick start bot

--- ctf_bot.py
+++ ctf_bot.py
@@ -1,10 +1,21 @@
 import discord
 from bot_token import TOKEN
 
+
 client = discord.Client()
 
+
 @client.event
-async def ready():
+async def on_ready():
     print(client.user)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "hello":
+        await message.channel.send("hahaha")
 
 client.run(TOKEN)
