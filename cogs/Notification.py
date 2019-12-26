78becc92eb5bf32715e0433d568fc4a9fb1ba4f5
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2019-12-27 00:21:14 +0900

    期間指定で開催するCTFを告知

--- cogs/Notification.py
+++ cogs/Notification.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone, timedelta
 from discord.ext import commands, tasks
 import requests
 import json
@@ -23,13 +24,30 @@ def get_event_api(args):
 
     target = target.format(limit, start, finish)
 
+    print("[+]: request link: {}".format(target))
     req = requests.get(target, headers=header)
 
     if req.status_code != 200:
         # todo: error handling
+        print("[+]: error occured. Status code is {}".format(req.status_code))
         return
 
     return json.loads(req.text)
+
+
+def date_format(date_string):
+    jst = timezone(timedelta(hours=9))
+    date_from_iso = datetime.fromisoformat(date_string)
+    new_date = date_from_iso.astimezone(jst)
+    return new_date.strftime("%Y-%m-%d(%a) %H:%M:%S")
+
+
+def cut_time_from_datetime(dt):
+    year = dt.year
+    month = dt.month
+    day = dt.day
+
+    return datetime(year, month, day)
 
 
 class Notification(commands.Cog):
@@ -40,7 +58,6 @@ class Notification(commands.Cog):
     @commands.command()
     async def next(self, ctx, *args):
         if len(args) == 0:
-            # そのうち複数対応にするかもしれないのでこうしてる
             limit = 1
         else:
             if args[0].isdecimal():
@@ -56,12 +73,73 @@ class Notification(commands.Cog):
 
         for ctf_data in ctfs:
             title = ctf_data["title"]
-            desc = ctf_data["description"]
             url = ctf_data["url"]
-            start = ctf_data["start"]
-            finish = ctf_data["finish"]
+            start = date_format(ctf_data["start"])
+            finish = date_format(ctf_data["finish"])
 
-            msg = "title: {}\ndescription: {}\nurl: {}\ndate: {} ~ {}".format(title, desc, url, start, finish)
+            msg = "title:\n\t{}\nurl:\n\t{}\ndate(JST):\n\t{} ~ {}".format(title, url, start, finish)
 
             await ctx.send(msg)
 
+
+    @commands.command()
+    async def today(self, ctx):
+        today_datetime = cut_time_from_datetime(datetime.today()) + timedelta(hours=9)
+        api_args = {
+            "limit": 10,
+            "start": int(today_datetime.timestamp()),
+            "finish": int((today_datetime + timedelta(hours=24)).timestamp())
+        }
+
+        ctfs = get_event_api(api_args)
+
+        if len(ctfs) == 0:
+            await ctx.send("No CTFs will be held today")
+
+        for ctf_data in ctfs:
+            msg = "-" * 100
+            msg += "\n"
+            title = ctf_data["title"]
+            url = ctf_data["url"]
+            start = date_format(ctf_data["start"])
+            finish = date_format(ctf_data["finish"])
+
+            msg += "title:\n\t{}\nurl:\n\t{}\ndate(JST):\n\t{} ~ {}".format(
+                title, url, start, finish)
+
+            await ctx.send(msg)
+
+            if len(ctfs) != 0:
+                await ctx.send("-" * 100)
+
+    
+    @commands.command()
+    async def week(self, ctx):
+        today_datetime = cut_time_from_datetime(
+            datetime.today()) + timedelta(hours=9)
+        api_args = {
+            "limit": 10,
+            "start": int(today_datetime.timestamp()),
+            "finish": int((today_datetime + timedelta(days=7)).timestamp())
+        }
+
+        ctfs = get_event_api(api_args)
+
+        if len(ctfs) == 0:
+            await ctx.send("No CTFs will be held this week")
+
+        for ctf_data in ctfs:
+            msg = "-" * 100
+            msg += "\n"
+            title = ctf_data["title"]
+            url = ctf_data["url"]
+            start = date_format(ctf_data["start"])
+            finish = date_format(ctf_data["finish"])
+
+            msg += "title:\n\t{}\nurl:\n\t{}\ndate(JST):\n\t{} ~ {}\n".format(
+                title, url, start, finish)
+
+            await ctx.send(msg)
+
+        if len(ctfs) != 0:
+            await ctx.send("-" * 100)
